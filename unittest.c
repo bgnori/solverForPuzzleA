32213941f4446a43c6ef60fae5d00723af347be8
Noriyuki Hosaka <bgnori@gmail.com>
2013-08-30 12:52:03 +0900

    starting out unittest 	modified:   unittest.c

--- unittest.c
+++ unittest.c
@@ -1,6 +1,31 @@
 #include "puzzle.h"
 
-bool
+static const char*
+test_fact_0()
+{
+    if (fact[0] != 1)
+        return __func__;
+    return NULL;
+}
+
+static const char*
+test_fact_1()
+{
+    if (fact[1] != 1)
+        return __func__;
+    return NULL;
+}
+
+static const char*
+test_fact_6()
+{
+    if (fact[6] != 720)
+        return __func__;
+    return NULL;
+}
+
+
+static bool
 test_perfhash(void)
 {
     int buf[9];
@@ -20,6 +45,7 @@ test_perfhash(void)
     }
     return ret;
 
+    // return __func__;
 }
 
 /*
@@ -39,14 +65,38 @@ test_perfhash(void)
         }
 */
 
+
+typedef const char* const_char_p;
+typedef const_char_p (*unittest)(void);
+
+unittest cases[] = {
+    test_fact_0,
+    test_fact_1,
+    test_fact_6,
+};
+
 int
-main(int argc, char** argv)
+main(int argc, const char** argv)
 {
-    printf("%d\n", test_perfhash());
+    int i, len;
+    const_char_p msg;
+    unittest u;
+
+    len = sizeof(cases)/sizeof(unittest);
+
+    for(i = 0; i < len; i++){
+        u = cases[i];
+        msg = u();
+        if(msg){
+            printf("!\n");
+            printf("fail, %s\n", msg);
+        }else{
+            printf(".");
+        }
+    }
+
     return 0;
 }
-
-
 
 
 
